fc0795c4725bcd5921a985c3c25215737ae871ea
dbkimball <dkimball311@msn.com>
2023-01-07 18:42:18 -0700

    week 1 programs

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}.");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("Congratulations, you have passed this course!");
+        }
+        else 
+        {
+            Console.WriteLine("Unfortunately you did not meet the requirements of a 70 or better to pass this course.  Better luck next time.");
+        }
+       
     }
 }
